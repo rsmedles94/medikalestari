27928a683cdba67e924b08368ad2ab051c033628
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-05-26 21:29:23 +0700

    update

--- components/PromoKesehatan.tsx
+++ components/PromoKesehatan.tsx
@@ -554,7 +554,7 @@ const PromoKesehatan = () => {
 
         <div className="relative z-10 max-w-[1190px] mx-auto md:px-8">
           <h2 className="text-3xl md:text-4xl font-bold text-white mb-8 -mt-10 text-center">
-            Promo Paket Kesehatan Medika Lestari
+            Penawaran Spesial Promo Paket Kesehatan Untuk Anda
           </h2>
           <div className="w-full overflow-hidden">
             <motion.div
@@ -576,7 +576,7 @@ const PromoKesehatan = () => {
                       key={item.id}
                       className="w-1/2 lg:w-1/4 shrink-0 p-2 md:p-3"
                     >
-                      <article className="bg-white border border-gray-100 flex flex-col h-full shadow-lg overflow-hidden transition-all duration-300">
+                      <article className="bg-white border border-gray-100 flex flex-col h-full shadow-lg overflow-hidden transition-all duration-300 hover:-translate-y-1 hover:shadow-xl">
                         <div className="relative aspect-square w-full overflow-hidden bg-gray-50">
                           <Image
                             src={item.image}
