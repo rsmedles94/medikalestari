f59efae86a49813feb6edaffecdb5142f77466a0
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-05-26 21:39:54 +0700

    update

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -567,8 +567,8 @@ const HeroSection = () => {
       </div>
 
       {/* Searchbar Desktop */}
-      <div className="hidden md:absolute md:inset-0 md:flex md:flex-col md:items-end md:justify-end md:z-20 md:pr-15 md:pb-15 md:pointer-events-none">
-        <div className="md:pointer-events-auto bg-[#003f88] rounded-md shadow-lg p-5">
+      <div className="hidden md:absolute md:inset-0 md:flex md:flex-col md:items-end md:justify-end md:z-20 md:pr-15 md:pb-32 md:pointer-events-none">
+        <div className="md:pointer-events-auto bg-[#003f88]  shadow-lg p-5">
           {/* titlr */}
           <h2 className="text-lg font-bold text-white mb-4">
             Cari Dokter Spesialis atau Jadwal Praktek
