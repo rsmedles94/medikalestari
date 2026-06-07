aa01d1de165c2c5a7268c47290fb8bb12250a8fa
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-06-07 14:41:23 +0700

    update

--- components/NavbarClient.tsx
+++ components/NavbarClient.tsx
@@ -333,7 +333,7 @@ const NavbarClient: React.FC<NavbarClientProps> = ({ logoNode }) => {
         <div className="py-4 relative">
           <div className="max-w-[1200px] mx-auto px-4 md:px-8 flex justify-between items-center">
             {/* Logo */}
-            <Link href="/" className="flex items-center scale-80 md:scale-125">
+            <Link href="/" className="flex items-center scale-80 md:scale-100">
               {logoNode}
             </Link>
 
