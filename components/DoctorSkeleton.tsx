a7ab09854ffce33ff5750778aadebbb3d8d1e431
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-06-12 12:40:59 +0700

    update

--- components/DoctorSkeleton.tsx
+++ components/DoctorSkeleton.tsx
@@ -29,8 +29,8 @@ const DoctorSkeleton = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
-      // Parent container background shimmer
-      className="relative flex flex-row items-start gap-4 md:gap-8 p-4 md:p-8 bg-slate-50 border border-slate-100 shadow-sm h-fit min-h-40 md:min-h-65 rounded-none overflow-hidden"
+      // Mengubah flex menjadi hidden md:flex agar sembunyi di mobile dan muncul di desktop
+      className="relative hidden md:flex flex-row items-start gap-4 md:gap-8 p-4 md:p-8 bg-slate-50 border border-slate-100 shadow-sm h-fit min-h-40 md:min-h-65 rounded-none overflow-hidden"
     >
       {/* Foto Dokter Skeleton shimmer effect */}
       <div className="relative w-24 h-24 sm:w-32 sm:h-32 md:w-44 md:h-44 shrink-0 rounded-full bg-slate-200 border-4 border-slate-50 overflow-hidden shadow-sm">
@@ -38,29 +38,19 @@ const DoctorSkeleton = () => {
       </div>
 
       {/* Konten Dokter Skeleton */}
-      <div className="flex-1 text-left pt-2 space-y-4">
+      <div className="flex-1 text-left pt-2 space-y-4 mt-6">
         {/* Nama -  shimmer effect */}
         <div className="relative h-6 md:h-8 bg-slate-300 w-3/4 overflow-hidden shadow-sm">
           <ShimmerOverlay />
         </div>
 
         {/* Spesialisasi -  shimmer effect */}
-        <div className="relative h-3 md:h-4 bg-slate-250  w-1/4 overflow-hidden shadow-sm">
+        <div className="relative h-3 md:h-4 bg-slate-250  w-1/4 overflow-hidden shadow-sm -mt-2">
           <ShimmerOverlay />
         </div>
 
-        {/* Bio (Desktop Only) -  shimmer effect */}
-        <div className="space-y-2 hidden md:block">
-          <div className="relative h-3 bg-slate-200 rounded-sm w-full overflow-hidden shadow-sm">
-            <ShimmerOverlay />
-          </div>
-          <div className="relative h-3 bg-slate-200 rounded-sm w-5/6 overflow-hidden shadow-sm">
-            <ShimmerOverlay />
-          </div>
-        </div>
-
         {/* Buttons shimmer */}
-        <div className="flex flex-wrap justify-start gap-2 md:gap-3 mt-4 md:mt-8">
+        <div className="flex flex-wrap justify-start gap-2 md:gap-3 ">
           <div className="relative h-9 md:h-11 w-28 md:w-36 bg-linear-to-r from-slate-200 to-slate-300 rounded-full overflow-hidden shadow-sm">
             <ShimmerOverlay />
           </div>
